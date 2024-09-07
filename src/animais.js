c463f5d80f5ef2b51f54e169f79e8cf13dfc1630
emersonjesuz <josiemerson2013@gmail.com>
2024-09-07 16:43:55 -0300

    feat: adicionar uma lista com todos os animais

---
+++ src/animais.js
@@ -0,0 +1,38 @@
+export const animais = [
+  {
+    especie: "LEAO",
+    tamanho: 3,
+    carnivero: true,
+    biomas: ["savana"],
+  },
+  {
+    especie: "LEOPARDO",
+    tamanho: 2,
+    carnivero: true,
+    biomas: ["savana"],
+  },
+  {
+    especie: "CROCODILO",
+    tamanho: 3,
+    carnivero: true,
+    biomas: ["rio"],
+  },
+  {
+    especie: "MACACO",
+    tamanho: 1,
+    carnivero: false,
+    biomas: ["savana", "floresta"],
+  },
+  {
+    especie: "GAZELA",
+    tamanho: 2,
+    carnivero: false,
+    biomas: ["savana"],
+  },
+  {
+    especie: "HIPOPOTAMO",
+    tamanho: 4,
+    carnivero: false,
+    biomas: ["savana", "rio"],
+  },
+];
