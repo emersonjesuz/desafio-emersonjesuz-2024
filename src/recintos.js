9edcc989cc3acc20fec0c37c26ab38eeeb83634f
emersonjesuz <josiemerson2013@gmail.com>
2024-09-07 16:43:18 -0300

    feat: adicionar uma lista com todos os recintos

---
+++ src/recintos.js
@@ -0,0 +1,53 @@
+export const recintos = [
+  {
+    numero: 1,
+    tamanhoTotal: 10,
+    biomas: ["savana"],
+    animais: [
+      {
+        especie: "MACACO",
+        quantidade: 3,
+        tamanho: 1,
+        carnivero: false,
+      },
+    ],
+  },
+  {
+    numero: 2,
+    tamanhoTotal: 5,
+    biomas: ["floresta"],
+    animais: [],
+  },
+  {
+    numero: 3,
+    tamanhoTotal: 7,
+    biomas: ["savana", "rio"],
+    animais: [
+      {
+        especie: "GAZELA",
+        quantidade: 1,
+        tamanho: 2,
+        carnivero: false,
+      },
+    ],
+  },
+  {
+    numero: 4,
+    tamanhoTotal: 8,
+    biomas: ["rio"],
+    animais: [],
+  },
+  {
+    numero: 5,
+    tamanhoTotal: 9,
+    biomas: ["savana"],
+    animais: [
+      {
+        especie: "LEAO",
+        quantidade: 1,
+        tamanho: 3,
+        carnivero: true,
+      },
+    ],
+  },
+];
